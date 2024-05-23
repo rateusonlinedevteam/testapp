9f47f33a48c400100e40be4143d0629badc3a1a6
rateusonline <106961091+rateusonline@users.noreply.github.com>
2024-05-23 11:49:23 +0530

    update the changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let msg = "initial changes now i make other changes in the files";
+let msg1 = "initial changes now i make other changes in the files";
+let msg2 = "initial changes now i make other changes in the files";
 
 console.log(msg);
